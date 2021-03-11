489c6e5b4ed84a7afb7343af22383bacd2b42865
Sam Marxz <sammarxzz@gmail.com>
2021-03-11 10:55:20 -0300

    Add typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repositories {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repositories[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/users/sammarxz/repos')
