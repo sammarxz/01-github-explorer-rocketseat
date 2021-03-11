b824f5286b458c12c13e676f0da4772c955f93d0
Sam Marxz <sammarxzz@gmail.com>
2021-03-11 08:47:49 -0300

    create RepositoriesList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,24 @@
+export function RepositoryList() {
+  return (
+    <section className="respository-list">
+      <h1>Lista de respositórios</h1>
+      <ul>
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição do repositório</p>
+          <a href="#" target="blank">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição do repositório</p>
+          <a href="#" target="blank">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>Nome do repositório</strong>
+          <p>Descrição do repositório</p>
+          <a href="#" target="blank">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
